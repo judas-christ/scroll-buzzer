ea0a4c71327919c33c651c155351742181e5ebd1
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2015-09-12 14:03:42 +0200

    First version

---
+++ scripts/scroll-buzzer.js
@@ -0,0 +1,36 @@
+(function(window) {
+  'use strict';
+
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  if(!AudioContext) return;
+
+  var document = window.document;
+
+  var audioContext = new AudioContext();
+  var osc = audioContext.createOscillator();
+  var gain = audioContext.createGain();
+  osc.connect(gain);
+  gain.connect(audioContext.destination);
+  gain.gain.value = 0;
+  osc.start();
+
+  var oldScrollValue = window.scrollY;
+  var newScrollValue, dScroll;
+  var scrollFreqMod = 20;
+  var scrollTimeout = null;
+  document.addEventListener('scroll', function(e) {
+    newScrollValue = window.scrollY;
+    dScroll = Math.abs(newScrollValue - oldScrollValue);
+    oldScrollValue = newScrollValue;
+
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function() {
+      gain.gain.value = 0;
+      oldScrollValue = window.scrollY;
+    }, 50);
+
+    gain.gain.value = 0.8;
+    osc.frequency.value = dScroll*scrollFreqMod+50;
+  }, false);
+
+})(this);
